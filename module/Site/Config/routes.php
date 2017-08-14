b300bafe8b44f701e53526a5bbc964892460924b
David Yang <daworld.ny@gmail.com>
2017-08-14 19:50:29 +0500

    Routes in standalone file

---
+++ module/Site/Config/routes.php
@@ -0,0 +1,69 @@
+<?php
+
+return array(
+	'/site/captcha/(:var)' => array(
+		'controller' => 'Site@captchaAction'
+	),
+
+	'/' => array(
+		'controller' => 'Site@indexAction'
+	),
+
+	'/hello/(:var)' => array(
+		'controller' => 'Site@helloAction',
+	),
+
+	'/contact' => array(
+		'controller' => 'Contact@indexAction'
+	),
+	
+	'/auth/login' => array(
+		'controller' => 'Auth@indexAction'
+	),
+	'/auth/logout' => array(
+		'controller' => 'Auth@logoutAction'
+	),
+	'/register' => array(
+		'controller' => 'Register@indexAction'
+	),
+
+	'/architecture' => array(
+		'controller' => 'Architecture:Grid@indexAction'
+	),
+
+	'/architecture/view/(:var)' => array(
+		'controller' => 'Architecture:Grid@floorAction'
+	),
+	
+	'/architecture/room/add' => array(
+		'controller' => 'Architecture:Room@saveAction'
+	),
+	
+	'/architecture/room/save' => array(
+		'controller' => 'Architecture:Room@saveAction'
+	),
+	
+	'/architecture/room/edit/(:var)' => array(
+		'controller' => 'Architecture:Room@editAction'
+	),
+
+	'/architecture/room/delete/(:var)' => array(
+		'controller' => 'Architecture:Room@deleteAction'
+	),
+
+	'/architecture/floor/add' => array(
+		'controller' => 'Architecture:Floor@saveAction'
+	),
+	
+	'/architecture/floor/save' => array(
+		'controller' => 'Architecture:Floor@saveAction'
+	),
+	
+	'/architecture/floor/edit/(:var)' => array(
+		'controller' => 'Architecture:Floor@editAction'
+	),
+
+	'/architecture/floor/delete/(:var)' => array(
+		'controller' => 'Architecture:Floor@deleteAction'
+	),
+);
--- module/Site/Module.php
+++ module/Site/Module.php
@@ -15,77 +15,10 @@ final class Module extends AbstractModule
      */
     public function getRoutes()
     {
-        return array(
-            '/site/captcha/(:var)' => array(
-                'controller' => 'Site@captchaAction'
-            ),
+		return include(__DIR__.'/config/routes.php');
+	}
 
-            '/' => array(
-                'controller' => 'Site@indexAction'
-            ),
-
-            '/hello/(:var)' => array(
-                'controller' => 'Site@helloAction',
-            ),
-
-            '/contact' => array(
-                'controller' => 'Contact@indexAction'
-            ),
-            
-            '/auth/login' => array(
-                'controller' => 'Auth@indexAction'
-            ),
-            '/auth/logout' => array(
-                'controller' => 'Auth@logoutAction'
-            ),
-            '/register' => array(
-                'controller' => 'Register@indexAction'
-            ),
-
-            '/architecture' => array(
-                'controller' => 'Architecture:Grid@indexAction'
-            ),
-
-            '/architecture/view/(:var)' => array(
-                'controller' => 'Architecture:Grid@floorAction'
-            ),
-            
-            '/architecture/room/add' => array(
-                'controller' => 'Architecture:Room@saveAction'
-            ),
-            
-            '/architecture/room/save' => array(
-                'controller' => 'Architecture:Room@saveAction'
-            ),
-            
-            '/architecture/room/edit/(:var)' => array(
-                'controller' => 'Architecture:Room@editAction'
-            ),
-
-            '/architecture/room/delete/(:var)' => array(
-                'controller' => 'Architecture:Room@deleteAction'
-            ),
-
-            '/architecture/floor/add' => array(
-                'controller' => 'Architecture:Floor@saveAction'
-            ),
-            
-            '/architecture/floor/save' => array(
-                'controller' => 'Architecture:Floor@saveAction'
-            ),
-            
-            '/architecture/floor/edit/(:var)' => array(
-                'controller' => 'Architecture:Floor@editAction'
-            ),
-
-            '/architecture/floor/delete/(:var)' => array(
-                'controller' => 'Architecture:Floor@deleteAction'
-            ),
-            
-        );
-    }
-
-    /**
+	/**
      * Returns prepared service instances of this module
      * 
      * @return array
