2c47b1a483b0bec6cdf6b4f05c467c581ca8d352
David Yang <daworld.ny@gmail.com>
2017-08-14 21:18:47 +0500

    Table for reservations

--- module/Site/Storage/MySQL/schema.sql
+++ module/Site/Storage/MySQL/schema.sql
@@ -9,3 +9,26 @@ CREATE TABLE hotelia_floor_room (
     `floor_id` INT NOT NULL,
     `name` varchar(255) NOT NULL
 );
+
+DROP TABLE IF EXISTS hotelia_reservation;
+CREATE TABLE hotelia_reservation (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`first_name` varchar(255) NOT NULL,
+	`last_name` varchar(255) NOT NULL,
+	`middle_name` varchar(255) NOT NULL,
+	`gender` varchar(1) NOT NULL,
+	`country` varchar(2) NOT NULL,
+	`status` varchar(1) NOT NULL,
+	`floor` SMALLINT NOT NULL,
+	`room` SMALLINT NOT NULL,
+	`includes` varchar(100),
+
+	/* Date */
+	`arrival` varchar(30) NOT NULL,
+	`departure` varchar(20) NOT NULL,
+
+	/* Food */
+	`breakfast` varchar(1) NOT NULL,
+	`dinner` varchar(1) NOT NULL,
+	`snack` varchar(1) NOT NULL
+);
