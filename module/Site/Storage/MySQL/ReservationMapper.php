977d7eef559ea3edabe11a967e4dbbdc40214950
David Yang <daworld.ny@gmail.com>
2017-08-26 16:41:49 +0500

    Render room value

--- module/Site/Storage/MySQL/ReservationMapper.php
+++ module/Site/Storage/MySQL/ReservationMapper.php
@@ -6,34 +6,54 @@ use Krystal\Db\Sql\AbstractMapper;
 
 final class ReservationMapper extends AbstractMapper
 {
-	/**
-	 * {@inheritDoc}
-	 */
-	public static function getTableName()
-	{
-		return self::getWithPrefix('hotelia_reservation');
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public static function getTableName()
+    {
+        return self::getWithPrefix('hotelia_reservation');
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	protected function getPk()
-	{
-		return 'id';
-	}
+    /**
+     * {@inheritDoc}
+     */
+    protected function getPk()
+    {
+        return 'id';
+    }
 
-	/**
-	 * Fetch all records
-	 * 
-	 * @return array
-	 */
-	public function fetchAll()
-	{
-		$db = $this->db->select('*')
-					   ->from(self::getTableName())
-					   ->orderBy('id')
-					   ->desc();
+    /**
+     * Fetch all records
+     * 
+     * @return array
+     */
+    public function fetchAll()
+    {
+        // Columns to be selected
+        $columns = array(
+            self::getFullColumnName('id'),
+            self::getFullColumnName('room_id'),
+            self::getFullColumnName('full_name'),
+            self::getFullColumnName('gender'),
+            self::getFullColumnName('country'),
+            self::getFullColumnName('status'),
+            self::getFullColumnName('arrival'),
+            self::getFullColumnName('departure'),
+            RoomMapper::getFullColumnName('name') => 'room'
+        );
 
-		return $db->queryAll();
-	}
+        $db = $this->db->select($columns)
+                       ->from(self::getTableName())
+                       // Room relation
+                       ->leftJoin(RoomMapper::getTableName())
+                       ->on()
+                       ->equals(
+                            self::getFullColumnName('room_id'),
+                            RoomMapper::getRawColumn('id')
+                       )
+                       ->orderBy('id')
+                       ->desc();
+
+        return $db->queryAll();
+    }
 }
