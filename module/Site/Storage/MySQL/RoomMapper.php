996208a19ef7eb9dce15b2944738f130aadb5f15
David Yang <daworld.ny@gmail.com>
2017-08-28 23:17:14 +0500

    Added fetchCleaning()

--- module/Site/Storage/MySQL/RoomMapper.php
+++ module/Site/Storage/MySQL/RoomMapper.php
@@ -32,6 +32,48 @@ final class RoomMapper extends AbstractMapper
     }
 
     /**
+     * Fetches cleaning data of rooms
+     * 
+     * @return array
+     */
+    public function fetchCleaning()
+    {
+        // Columns to be selected
+        $columns = array(
+            self::getFullColumnName('id'),
+            self::getFullColumnName('floor_id'),
+            self::getFullColumnName('type_id'),
+            self::getFullColumnName('persons'),
+            self::getFullColumnName('name'),
+            self::getFullColumnName('square'),
+            self::getFullColumnName('quality'),
+            self::getFullColumnName('cleaned'),
+            RoomTypeMapper::getFullColumnName('type'),
+            FloorMapper::getFullColumnName('name') => 'floor'
+        );
+
+        return $this->db->select($columns)
+                        ->from(self::getTableName())
+                        // Type relation
+                        ->leftJoin(RoomTypeMapper::getTableName())
+                        ->on()
+                        ->equals(
+                            self::getFullColumnName('type_id'),
+                            RoomTypeMapper::getRawColumn('id')
+                        )
+                        // Floor relation
+                        ->leftJoin(FloorMapper::getTableName())
+                        ->on()
+                        ->equals(
+                            self::getFullColumnName('floor_id'),
+                            FloorMapper::getRawColumn('id')
+                        )
+                        ->orderBy(array(FloorMapper::getFullColumnName('name')))
+                        ->desc()
+                        ->queryAll();
+    }
+
+    /**
      * Fetch all rooms by associated floor ID
      * 
      * @param string $floorId
