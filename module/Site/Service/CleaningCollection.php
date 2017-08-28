56ed44892c3d5f64a0ae1089ce6f40f1f36a20e6
David Yang <daworld.ny@gmail.com>
2017-08-28 23:17:31 +0500

    Keys as constants

--- module/Site/Service/CleaningCollection.php
+++ module/Site/Service/CleaningCollection.php
@@ -6,13 +6,18 @@ use Krystal\Stdlib\ArrayCollection;
 
 final class CleaningCollection extends ArrayCollection
 {
+    const CODE_UNDEFINED = '0';
+    const CODE_CLEANED = '1';
+    const CODE_NONCLEANED = '2';
+    const CODE_CHECKING = '3';
+
     /**
      * {@inheritDoc}
      */
     protected $collection = array(
-        '0' => 'Undefined',
-        '1' => 'Cleaned',
-        '2' => 'Not cleaned',
-        '3' => 'Checking'
+        self::CODE_UNDEFINED => 'Undefined',
+        self::CODE_CLEANED => 'Cleaned',
+        self::CODE_NONCLEANED => 'Not cleaned',
+        self::CODE_CHECKING => 'Checking'
     );
 }
