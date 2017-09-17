eaaf73abeb7aea4d09b983c699d1858b29165728
David Yang <daworld.ny@gmail.com>
2017-09-17 15:45:37 +0500

    Added fetchAll() in HotelMapper

--- module/Site/Storage/MySQL/HotelMapper.php
+++ module/Site/Storage/MySQL/HotelMapper.php
@@ -30,4 +30,26 @@ final class HotelMapper extends AbstractMapper
     {
         return 'id';
     }
+
+    /**
+     * Fetch all hotels
+     * 
+     * @return array
+     */
+    public function fetchAll()
+    {
+        return $this->db->select('*')
+                        ->from(self::getTableName())
+                        ->orderBy($this->getPk())
+                        ->desc()
+                        ->asManyToMany(
+                            'facilities', 
+                            FacilitiyItemMapper::getJunctionTableName(), 
+                            self::PARAM_JUNCTION_MASTER_COLUMN, 
+                            FacilitiyItemMapper::getTableName(), 
+                            'id', 
+                            'name' // Columns to be selected in Service table
+                        )
+                        ->queryAll();
+    }
 }
