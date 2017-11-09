c40d1eaeb0b8b6587cfd6926759e61bdcd73f592
David Yang <daworld.ny@gmail.com>
2017-11-09 22:25:56 +0500

    Added DaysCollection

---
+++ module/Site/Service/DaysCollection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Site\Service;
+
+use Krystal\Stdlib\ArrayCollection;
+
+final class DaysCollection extends ArrayCollection
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $collection = array(
+        '7' => 'Week',
+        '14' => 'Two weeks',
+        '21' => 'Three weeks',
+        '31' => 'Month'
+    );
+}
