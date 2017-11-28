6f197079490aaa9b67cc99889e80aed48f2e1d25
David Yang <daworld.ny@gmail.com>
2017-11-28 15:46:40 +0500

    Basic imlementation of stat

--- module/Site/Controller/Stat.php
+++ module/Site/Controller/Stat.php
@@ -2,6 +2,8 @@
 
 namespace Site\Controller;
 
+use Krystal\Date\TimeHelper;
+
 final class Stat extends AbstractCrmController
 {
     /**
@@ -11,14 +13,24 @@ final class Stat extends AbstractCrmController
      */
     public function indexAction()
     {
-    }
+        if ($this->request->isPost()) {
+            $year = $this->request->getPost('year');
+            $months = $this->request->getPost('months');
+            $rooms = $this->request->getPost('rooms');
 
-    /**
-     * Counts statistic based on form parameters
-     * 
-     * @return string
-     */
-    public function countAction()
-    {
+            $mapper = $this->createMapper('\Site\Storage\MySQL\ReservationMapper');
+            $sum = $mapper->getSumCount($year, $months, $rooms);
+            
+            return $this->view->disableLayout()->render('stat/response', [
+                'sum' => $sum
+            ]);
+
+        } else {
+            return $this->view->render('stat/index', [
+                'months' => TimeHelper::getMonths(),
+                'year' => date('Y'),
+                'rooms' => $this->getModuleService('architectureService')->createRooms($this->getHotelId())
+            ]);
+        }
     }
 }
