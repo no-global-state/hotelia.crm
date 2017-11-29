fe1c60b9397c53cc7b8d1a5ccc19b4d99b7dc791
David Yang <daworld.ny@gmail.com>
2017-11-29 13:09:20 +0500

    Updated syntax

--- module/Site/Config/routes.php
+++ module/Site/Config/routes.php
@@ -1,314 +1,313 @@
 <?php
 
-return array(
-
-    '/' => array(
+return [
+    '/' => [
         'controller' => 'Site@homeAction'
-    ),
+    ],
     
-    '/crm' => array(
+    '/crm' => [
         'controller' => 'Crm@indexAction'
-    ),
+    ],
 
-    '/crm/languages/switch/(:var)' => array(
+    '/crm/languages/switch/(:var)' => [
         'controller' => 'Language@switchAction'
-    ),
+    ],
     
-    '/crm/languages' => array(
+    '/crm/languages' => [
         'controller' => 'Language@indexAction'
-    ),
+    ],
     
-    '/crm/languages/save' => array(
+    '/crm/languages/save' => [
         'controller' => 'Language@saveAction'
-    ),
+    ],
 
-    '/crm/languages/edit/(:var)' => array(
+    '/crm/languages/edit/(:var)' => [
         'controller' => 'Language@editAction'
-    ),
+    ],
     
-    '/crm/languages/delete/(:var)' => array(
+    '/crm/languages/delete/(:var)' => [
         'controller' => 'Language@deleteAction'
-    ),
+    ],
     
-    '/payment' => array(
+    '/payment' => [
         'controller' => 'Site@paymentAction'
-    ),
+    ],
     
-    '/search/(:var)' => array(
+    '/search/(:var)' => [
         'controller' => 'Site@searchAction'
-    ),
+    ],
     
-    '/hotel/book/(:var)' => array(
+    '/hotel/book/(:var)' => [
         'controller' => 'Site@bookAction'
-    ),
+    ],
 
-    '/hotel/(:var)' => array(
+    '/hotel/(:var)' => [
         'controller' => 'Site@hotelAction'
-    ),
+    ],
     
-    '/site/captcha/(:var)' => array(
+    '/site/captcha/(:var)' => [
         'controller' => 'Site@captchaAction'
-    ),
+    ],
 
-    '/crm/home' => array(
+    '/crm/home' => [
         'controller' => 'Crm@indexAction'
-    ),
+    ],
 
-    '/auth/login' => array(
+    '/auth/login' => [
         'controller' => 'Auth@indexAction'
-    ),
+    ],
 
-    '/auth/logout' => array(
+    '/auth/logout' => [
         'controller' => 'Auth@logoutAction'
-    ),
+    ],
 
-    '/register' => array(
+    '/register' => [
         'controller' => 'Register@indexAction'
-    ),
+    ],
 
-    '/crm/photo/add' => array(
+    '/crm/photo/add' => [
         'controller' => 'Photo@addAction'
-    ),
+    ],
 
-    '/crm/photo/save' => array(
+    '/crm/photo/save' => [
         'controller' => 'Photo@saveAction'
-    ),
+    ],
 
-    '/crm/photo/edit/(:var)' => array(
+    '/crm/photo/edit/(:var)' => [
         'controller' => 'Photo@editAction'
-    ),
+    ],
 
-    '/crm/photo/delete/(:var)' => array(
+    '/crm/photo/delete/(:var)' => [
         'controller' => 'Photo@deleteAction'
-    ),
+    ],
 
-    '/crm/facility' => array(
+    '/crm/facility' => [
         'controller' => 'Facility:Grid@indexAction'
-    ),
+    ],
 
-    '/crm/facility/checklist' => array(
+    '/crm/facility/checklist' => [
         'controller' => 'Facility:Grid@checklistAction'
-    ),
+    ],
 
-    '/crm/facility/category/view/(:var)' => array(
+    '/crm/facility/category/view/(:var)' => [
         'controller' => 'Facility:Grid@categoryAction'
-    ),
+    ],
 
     // Categories
-    '/crm/facility/category/add' => array(
+    '/crm/facility/category/add' => [
         'controller' => 'Facility:Category@addAction'
-    ),
+    ],
 
-    '/crm/facility/category/save' => array(
+    '/crm/facility/category/save' => [
         'controller' => 'Facility:Category@saveAction'
-    ),
+    ],
 
-    '/crm/facility/category/edit/(:var)' => array(
+    '/crm/facility/category/edit/(:var)' => [
         'controller' => 'Facility:Category@editAction'
-    ),
+    ],
 
-    '/crm/facility/category/delete/(:var)' => array(
+    '/crm/facility/category/delete/(:var)' => [
         'controller' => 'Facility:Category@deleteAction'
-    ),
+    ],
 
     // Item
-    '/crm/facility/item/add' => array(
+    '/crm/facility/item/add' => [
         'controller' => 'Facility:Item@addAction'
-    ),
+    ],
 
-    '/crm/facility/item/save' => array(
+    '/crm/facility/item/save' => [
         'controller' => 'Facility:Item@saveAction'
-    ),
+    ],
 
-    '/crm/facility/item/edit/(:var)' => array(
+    '/crm/facility/item/edit/(:var)' => [
         'controller' => 'Facility:Item@editAction'
-    ),
+    ],
 
-    '/crm/facility/item/delete/(:var)' => array(
+    '/crm/facility/item/delete/(:var)' => [
         'controller' => 'Facility:Item@deleteAction'
-    ),
+    ],
 
-    '/crm/transaction/index/(:var)' => array(
+    '/crm/transaction/index/(:var)' => [
         'controller' => 'Transaction@indexAction'
-    ),
+    ],
 
-    '/crm/hotel' => array(
+    '/crm/hotel' => [
         'controller' => 'Hotel@indexAction'
-    ),
+    ],
 
-    '/crm/hotel/save' => array(
+    '/crm/hotel/save' => [
         'controller' => 'Hotel@saveAction'
-    ),
+    ],
 
-    '/crm/services' => array(
+    '/crm/services' => [
         'controller' => 'Service@indexAction'
-    ),
+    ],
     
-    '/crm/services/edit/(:var)' => array(
+    '/crm/services/edit/(:var)' => [
         'controller' => 'Service@editAction'
-    ),
+    ],
 
-    '/crm/services/delete/(:var)' => array(
+    '/crm/services/delete/(:var)' => [
         'controller' => 'Service@deleteAction'
-    ),
+    ],
 
-    '/crm/services/save' => array(
+    '/crm/services/save' => [
         'controller' => 'Service@saveAction'
-    ),
+    ],
 
     // Inventory
-    '/crm/inventory' => array(
+    '/crm/inventory' => [
         'controller' => 'Inventory@indexAction'
-    ),
+    ],
 
-    '/crm/inventory/edit/(:var)' => array(
+    '/crm/inventory/edit/(:var)' => [
         'controller' => 'Inventory@editAction'
-    ),
+    ],
 
-    '/crm/inventory/delete/(:var)' => array(
+    '/crm/inventory/delete/(:var)' => [
         'controller' => 'Inventory@deleteAction'
-    ),
+    ],
     
-    '/crm/inventory/save' => array(
+    '/crm/inventory/save' => [
         'controller' => 'Inventory@saveAction'
-    ),
+    ],
 
-    '/crm/reservation/table/(:var)' => array(
+    '/crm/reservation/table/(:var)' => [
         'controller' => 'Reservation@tableAction'
-    ),
+    ],
 
-    '/crm/reservation/find' => array(
+    '/crm/reservation/find' => [
         'controller' => 'Reservation@findAction'
-    ),
+    ],
     
-    '/crm/reservation/table/taken/(:var)' => array(
+    '/crm/reservation/table/taken/(:var)' => [
         'controller' => 'Reservation@viewTakenAction'
-    ),
+    ],
 
     // Reservation add
-    '/crm/reservation/add/(:var)' => array(
+    '/crm/reservation/add/(:var)' => [
         'controller' => 'Reservation@addAction'
-    ),
+    ],
 
-    '/crm/reservation/save' => array(
+    '/crm/reservation/save' => [
         'controller' => 'Reservation@saveAction'
-    ),
+    ],
 
-    '/crm/reservation/view/(:var)' => array(
+    '/crm/reservation/view/(:var)' => [
         'controller' => 'Reservation@viewAction'
-    ),
+    ],
 
-    '/crm/reservation/print/(:var)' => array(
+    '/crm/reservation/print/(:var)' => [
         'controller' => 'Reservation@printAction'
-    ),
+    ],
 
-    '/crm/reservation/index/(:var)' => array(
+    '/crm/reservation/index/(:var)' => [
         'controller' => 'Reservation@indexAction'
-    ),
+    ],
 
-    '/crm/reservation/edit/(:var)' => array(
+    '/crm/reservation/edit/(:var)' => [
         'controller' => 'Reservation@editAction'
-    ),
+    ],
     
-    '/crm/reservation/delete/(:var)' => array(
+    '/crm/reservation/delete/(:var)' => [
         'controller' => 'Reservation@deleteAction'
-    ),
+    ],
 
-    '/crm/reservation/history/room/(:var)' => array(
+    '/crm/reservation/history/room/(:var)' => [
         'controller' => 'Reservation@historyAction'
-    ),
+    ],
 
     // Room cleaning
-    '/crm/architecture/room-cleaning' => array(
+    '/crm/architecture/room-cleaning' => [
         'controller' => 'Architecture:RoomCleaning@indexAction'
-    ),
+    ],
 
-    '/crm/architecture/room-cleaning/room/(:var)/mark/(:var)' => array(
+    '/crm/architecture/room-cleaning/room/(:var)/mark/(:var)' => [
         'controller' => 'Architecture:RoomCleaning@markAction'
-    ),
+    ],
 
-    '/crm/architecture/room-cleaning/mark-batch/(:var)' => array(
+    '/crm/architecture/room-cleaning/mark-batch/(:var)' => [
         'controller' => 'Architecture:RoomCleaning@markBatchAction'
-    ),
+    ],
 
-    '/crm/architecture/room-type' => array(
+    '/crm/architecture/room-type' => [
         'controller' => 'Architecture:RoomType@indexAction'
-    ),
+    ],
 
-    '/crm/architecture/room-type/edit/(:var)' => array(
+    '/crm/architecture/room-type/edit/(:var)' => [
         'controller' => 'Architecture:RoomType@editAction'
-    ),
+    ],
 
-    '/crm/architecture/room-type/delete/(:var)' => array(
+    '/crm/architecture/room-type/delete/(:var)' => [
         'controller' => 'Architecture:RoomType@deleteAction'
-    ),
+    ],
 
-    '/crm/architecture/room-type/save' => array(
+    '/crm/architecture/room-type/save' => [
         'controller' => 'Architecture:RoomType@saveAction'
-    ),
+    ],
 
-    '/crm/architecture' => array(
+    '/crm/architecture' => [
         'controller' => 'Architecture:Grid@indexAction'
-    ),
+    ],
 
-    '/crm/architecture/view/(:var)' => array(
+    '/crm/architecture/view/(:var)' => [
         'controller' => 'Architecture:Grid@floorAction'
-    ),
+    ],
 
-    '/crm/architecture/room/add' => array(
+    '/crm/architecture/room/add' => [
         'controller' => 'Architecture:Room@addAction'
-    ),
+    ],
 
-    '/crm/architecture/room/view/(:var)' => array(
+    '/crm/architecture/room/view/(:var)' => [
         'controller' => 'Architecture:Room@viewAction'
-    ),
+    ],
 
-    '/crm/architecture/room/save' => array(
+    '/crm/architecture/room/save' => [
         'controller' => 'Architecture:Room@saveAction'
-    ),
+    ],
 
-    '/crm/architecture/room/edit/(:var)' => array(
+    '/crm/architecture/room/edit/(:var)' => [
         'controller' => 'Architecture:Room@editAction'
-    ),
+    ],
 
-    '/crm/architecture/room/delete/(:var)' => array(
+    '/crm/architecture/room/delete/(:var)' => [
         'controller' => 'Architecture:Room@deleteAction'
-    ),
+    ],
 
     // Room inventory
-    '/crm/architecture/room/(:var)/inventory' => array(
+    '/crm/architecture/room/(:var)/inventory' => [
         'controller' => 'Architecture:RoomInventory@indexAction'
-    ),
+    ],
 
-    '/crm/architecture/room/(:var)/inventory/edit/(:var)' => array(
+    '/crm/architecture/room/(:var)/inventory/edit/(:var)' => [
         'controller' => 'Architecture:RoomInventory@editAction'
-    ),
+    ],
 
-    '/crm/architecture/room/inventory/save' => array(
+    '/crm/architecture/room/inventory/save' => [
         'controller' => 'Architecture:RoomInventory@saveAction'
-    ),
+    ],
 
-    '/crm/architecture/room/(:var)/inventory/delete/(:var)' => array(
+    '/crm/architecture/room/(:var)/inventory/delete/(:var)' => [
         'controller' => 'Architecture:RoomInventory@deleteAction'
-    ),
+    ],
 
-    '/crm/architecture/floor/add' => array(
+    '/crm/architecture/floor/add' => [
         'controller' => 'Architecture:Floor@addAction'
-    ),
+    ],
 
-    '/crm/architecture/floor/save' => array(
+    '/crm/architecture/floor/save' => [
         'controller' => 'Architecture:Floor@saveAction'
-    ),
+    ],
 
-    '/crm/architecture/floor/edit/(:var)' => array(
+    '/crm/architecture/floor/edit/(:var)' => [
         'controller' => 'Architecture:Floor@editAction'
-    ),
+    ],
 
-    '/crm/architecture/floor/delete/(:var)' => array(
+    '/crm/architecture/floor/delete/(:var)' => [
         'controller' => 'Architecture:Floor@deleteAction'
-    ),
+    ],
 
-    '/crm/stat' => array(
+    '/crm/stat' => [
         'controller' => 'Stat@indexAction'
-    )
-);
+    ]
+];
