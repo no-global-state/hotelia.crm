387329a8646331d2a438fdb26008b40910ea6265
David Yang <daworld.ny@gmail.com>
2017-12-15 00:59:00 +0500

    Added add() method

--- module/Site/Storage/MySQL/RoomTypePriceMapper.php
+++ module/Site/Storage/MySQL/RoomTypePriceMapper.php
@@ -21,4 +21,39 @@ final class RoomTypePriceMapper extends AbstractMapper
     {
         return 'id';
     }
+
+    /**
+     * Adds room type price
+     * 
+     * @param int $roomTypeId
+     * @param array $priceGroupIds
+     * @return boolean
+     */
+    public function add(int $roomTypeId, array $priceGroupIds)
+    {
+        foreach ($priceGroupIds as $priceGroupId => $price) {
+            $this->insert($roomTypeId, $priceGroupId, $price);
+        }
+
+        return true;
+    }
+
+    /**
+     * Adds new item
+     * 
+     * @param int $roomTypeId
+     * @param int $priceGroupId
+     * @param float $price
+     * @return boolean
+     */
+    private function insert(int $roomTypeId, int $priceGroupId, float $price)
+    {
+        $data = [
+            'room_type_id' => $roomTypeId,
+            'price_group_id' => $priceGroupId,
+            'price' => $price
+        ];
+
+        return $this->persist($data);
+    }
 }
