4876df86e4ded1c79c0c8a87d2d5a797eb034fa8
David Yang <daworld.ny@gmail.com>
2017-12-16 22:56:11 +0500

    Added language service

---
+++ module/Site/Service/LanguageService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Site\Service;
+
+use Krystal\Db\Filter\InputDecorator;
+
+final class LanguageService
+{
+    /**
+     * Finds entity in collection by associated language ID
+     * 
+     * @param string $languageId
+     * @param mixed $entity
+     * @return mixed
+     */
+    public static function findByLangId($languageId, $entity)
+    {
+        if (empty($entity)) {
+            return new InputDecorator;
+        }
+
+        // Find attached entity
+        foreach ($entity as $translation) {
+            if ($translation['lang_id'] == $languageId) {
+                return $translation;
+            }
+        }
+
+        $input = new InputDecorator();
+        $input['id'] = $entity[0]['id'];
+
+        return $input;
+    }
+}
