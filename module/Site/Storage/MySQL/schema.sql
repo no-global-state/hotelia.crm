03f587ecd8b18049b3d4507d56780ece2775056c
David Yang <daworld.ny@gmail.com>
2017-12-18 12:36:56 +0500

    Added FK for room types

--- module/Site/Storage/MySQL/schema.sql
+++ module/Site/Storage/MySQL/schema.sql
@@ -132,8 +132,10 @@ CREATE TABLE velveto_floor_room (
 
 CREATE TABLE velveto_floor_room_types (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `hotel_id` INT DEFAULT 1 COMMENT 'Attached hotel ID',
-    `type` varchar(255) NOT NULL
+    `hotel_id` INT NOT NULL COMMENT 'Attached hotel ID',
+    `type` varchar(255) NOT NULL,
+
+    FOREIGN KEY (hotel_id) REFERENCES velveto_hotels(id) ON DELETE CASCADE
 );
 
 CREATE TABLE velveto_floor_room_type_prices (
