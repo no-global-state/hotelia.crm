b8351910a993a3e955a3b669202daa95127929c3
David Yang <daworld.ny@gmail.com>
2017-12-20 15:33:54 +0500

    Updated region mapper to handle internalization

--- module/Site/Storage/MySQL/RegionMapper.php
+++ module/Site/Storage/MySQL/RegionMapper.php
@@ -13,16 +13,52 @@ final class RegionMapper extends AbstractMapper
     }
 
     /**
-     * Fetch all regions
+     * {@inheritDoc}
+     */
+    public static function getTranslationTable()
+    {
+        return RegionTranslationMapper::getTableName();
+    }
+
+    /**
+     * Returns shared columns to be selected
      * 
      * @return array
      */
-    public function fetchAll() : array
+    private function getColumns() : array
     {
-        return $this->db->select('*')
-                        ->from(self::getTableName())
-                        ->orderBy($this->getPk())
-                        ->desc()
-                        ->queryAll();
+        return [
+            self::getFullColumnName('id'),
+            self::getFullColumnName('order'),
+            RegionTranslationMapper::getFullColumnName('lang_id'),
+            RegionTranslationMapper::getFullColumnName('name'),
+        ];
+    }
+
+    /**
+     * Fetch region by its ID
+     * 
+     * @param int $id Region ID
+     * @param int $langId Language ID filter
+     * @return array
+     */
+    public function fetchById(int $id, int $langId = 0)
+    {
+        return $this->findEntity($this->getColumns(), $id, $langId);
+    }
+
+    /**
+     * Fetch all regions
+     * 
+     * @param int $langId Language ID filter
+     * @return array
+     */
+    public function fetchAll(int $langId) : array
+    {
+        return $this->createEntitySelect($this->getColumns())
+                    ->whereEquals(RegionTranslationMapper::getFullColumnName('lang_id'), $langId)
+                    ->orderBy($this->getPk())
+                    ->desc()
+                    ->queryAll();
     }
 }
