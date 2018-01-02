2b985935dc5a1d40354e608013442b064bbf968e
David Yang <daworld.ny@gmail.com>
2018-01-02 17:49:40 +0500

    Added extra method to normalize single rooms with many reservation dates

--- module/Site/Service/ReservationService.php
+++ module/Site/Service/ReservationService.php
@@ -5,10 +5,79 @@ namespace Site\Service;
 use DateTime;
 use DateInterval;
 use DatePeriod;
+use LogicException;
 
 class ReservationService
 {
     const PARAM_TIME_FORMAT = 'Y-m-d';
+
+    /**
+     * Find reservation dates from collection of rooms and group them
+     * 
+     * @param string $pk Primary column item name
+     * @param array $rows A collection of items to be overated over
+     * @param array $columns Columns to be considered
+     * @return array
+     */
+    private static function findReservations(string $pk, array $rows, array $columns) : array
+    {
+        $seen = [];
+
+        foreach ($rows as $row) {
+            $id = $row[$pk];
+
+            if (!in_array($id, $row)) {
+                $seen[$id] = [];
+            }
+
+            foreach ($columns as $column) {
+                if (!isset($seen[$id][$column])) {
+                    $seen[$id][$column] = [];
+                }
+
+                $seen[$id][$column][] = $row[$column];
+            }
+        }
+
+        return $seen;
+    }
+
+    /**
+     * Parse raw set of room for format of the chessboard
+     * 
+     * @param array $rooms
+     * @return array
+     */
+    public static function parseRooms(array $rooms) : array
+    {
+        $output = [];
+        $columns = ['arrival', 'departure'];
+
+        // Find reservations
+        $reservations = self::findReservations('id', $rooms, $columns);
+
+        foreach ($rooms as $index => $room) {
+            $id = $room['id'];
+
+            if (!isset($output[$id])) {
+                // Remove columns in output
+                foreach($columns as $column){
+                    unset($room[$column]);
+                }
+
+                $output[$id] = $room;
+
+                if (isset($reservations[$id])) {
+                    $output[$id]['reservations'] = $reservations[$id];
+                }
+
+            } else {
+                continue;
+            }
+        }
+
+        return $output;
+    }
 
     /**
      * Returns today format
@@ -52,6 +121,35 @@ class ReservationService
     }
 
     /**
+     * Checks whether two dates
+     * 
+     * @param string $date
+     * @param array $arrival Arrival dates
+     * @param array $departures Departure dates
+     * @throws \LogicException If count of items in arrivals and departures is different
+     * @return boolean
+     */
+    public static function isDateInRanges(string $date, array $arrivals, array $departures) : bool
+    {
+        $arrivalCount = count($arrivals);
+        $departureCount = count($departures);
+
+        if ($arrivalCount !== $departureCount) {
+            throw new LogicException('Arrival and departure dates must have the same count of items');
+        }
+
+        $count = $arrivalCount; // Or $departureCount, it doesn't matter because their count is equal
+
+        for ($i = 0; $i < $count; ++$i) {
+            if (self::isDateInRange($date, $arrivals[$i], $departures[$i])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
      * Checks whether date is in range between two another dates
      * 
      * @param string $target
@@ -59,7 +157,7 @@ class ReservationService
      * @param string $end
      * @return boolean
      */
-    public static function isDateInRange($target, $start, $end)
+    private static function isDateInRange($target, $start, $end)
     {
         $date = new DateTime($target);
         $startDate = new DateTime($start);
