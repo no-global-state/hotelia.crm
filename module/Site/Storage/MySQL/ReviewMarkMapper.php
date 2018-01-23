4ade372edf844f65294594449522a8ffd73539af
dave-yang <daworld.ny@gmail.com>
2018-01-23 18:42:32 +0500

    Added findAllByReviewId()

--- module/Site/Storage/MySQL/ReviewMarkMapper.php
+++ module/Site/Storage/MySQL/ReviewMarkMapper.php
@@ -11,4 +11,31 @@ final class ReviewMarkMapper extends AbstractMapper
     {
         return self::getWithPrefix('velveto_reviews_marks');
     }
+
+    /**
+     * Find all marks by associated review ID
+     * 
+     * @param int $reviewId
+     * @return array
+     */
+    public function findAllByReviewId(int $reviewId) : array
+    {
+        // Columns to be selected
+        $columns = [
+            self::getFullColumnName('id'),
+            self::getFullColumnName('review_id'),
+            self::getFullColumnName('review_type_id'),
+            self::getFullColumnName('mark'),
+            ReviewTypeMapper::getFullColumnName('name') => 'type'
+        ];
+
+        return $this->db->select($columns)
+                        ->from(self::getTableName())
+                        // Review type relation
+                        ->leftJoin(ReviewTypeMapper::getTableName(), [
+                            ReviewTypeMapper::getFullColumnName('id') => self::getFullColumnName('review_type_id')
+                        ])
+                        ->whereEquals(self::getFullColumnName('review_id'), $reviewId)
+                        ->queryAll();
+    }
 }
