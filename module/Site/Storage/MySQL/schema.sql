1745900182fdc52cd8b80f05890256b9935a3d0b
dave-yang <daworld.ny@gmail.com>
2018-02-10 17:43:29 +0500

    Fixed typos in table names

--- module/Site/Storage/MySQL/schema.sql
+++ module/Site/Storage/MySQL/schema.sql
@@ -164,7 +164,7 @@ CREATE TABLE velveto_facilitiy_items_translation (
     FOREIGN KEY (id) REFERENCES velveto_facilitiy_items(id) ON DELETE CASCADE
 );
 
-CREATE TABLE velveto_facilitiy_items_data (
+CREATE TABLE velveto_facility_items_data (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `item_id` INT NOT NULL COMMENT 'Attched item ID',
     `order` INT NOT NULL COMMENT 'Sorting order',
@@ -172,13 +172,13 @@ CREATE TABLE velveto_facilitiy_items_data (
     FOREIGN KEY (item_id) REFERENCES velveto_facilitiy_items(id) ON DELETE CASCADE
 );
 
-CREATE TABLE velveto_facilitiy_items_data_translation (
+CREATE TABLE velveto_facility_items_data_translation (
     `id` INT NOT NULL,
     `lang_id` INT NOT NULL,
     `name` varchar(255) NOT NULL,
 
     FOREIGN KEY (lang_id) REFERENCES velveto_languages(id) ON DELETE CASCADE,
-    FOREIGN KEY (id) REFERENCES velveto_facilitiy_items_data(id) ON DELETE CASCADE
+    FOREIGN KEY (id) REFERENCES velveto_facility_items_data(id) ON DELETE CASCADE
 );
 
 CREATE TABLE velveto_hotels_photos (
