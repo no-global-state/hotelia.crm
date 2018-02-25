3842c4a42fac6654e78d371a0876d602d0395d67
dave-yang <daworld.ny@gmail.com>
2018-02-26 01:51:04 +0500

    Added findEvents()

--- module/Site/Storage/MySQL/ScheduleMapper.php
+++ module/Site/Storage/MySQL/ScheduleMapper.php
@@ -6,4 +6,24 @@ use Krystal\Db\Sql\RawSqlFragment;
 
 final class ScheduleMapper extends AbstractMapper
 {
+    /**
+     * Find all events by hotel it and date ranges
+     * 
+     * @param int $hotelId
+     * @param string $arrival
+     * @param string $departure
+     * @return array
+     */
+    public function findEvents(int $hotelId, string $arrival, string $departure) : array
+    {
+        $query = sprintf('SELECT * FROM %s WHERE NOT ((departure <= :arrival) OR (arrival >= :departure)) AND hotel_id = :hotelId', ReservationMapper::getTableName());
+        $bindings = [
+            ':arrival' => $arrival,
+            ':departure' => $departure,
+            ':hotelId' => $hotelId
+        ];
+
+        return $this->db->raw($query, $bindings)
+                        ->queryAll();
+    }
 }
