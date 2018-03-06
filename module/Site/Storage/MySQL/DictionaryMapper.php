37adcf3a57ed6b12eed5cd4e913b0e13af9e7719
dave-yang <daworld.ny@gmail.com>
2018-03-06 16:16:34 +0500

    Added fetch methods

--- module/Site/Storage/MySQL/DictionaryMapper.php
+++ module/Site/Storage/MySQL/DictionaryMapper.php
@@ -19,4 +19,46 @@ final class DictionaryMapper extends AbstractMapper
     {
         return DictionaryTranslationMapper::getTableName();
     }
+
+    /**
+     * Returns shared columns to be selected
+     * 
+     * @return array
+     */
+    private function getColumns() : array
+    {
+        return [
+            self::getFullColumnName('id'),
+            self::getFullColumnName('alias'),
+            DictionaryTranslationMapper::getFullColumnName('lang_id'),
+            DictionaryTranslationMapper::getFullColumnName('value'),
+        ];
+    }
+
+    /**
+     * Fetch dictionary entry by its ID
+     * 
+     * @param int $id Entry ID
+     * @param int $langId Language ID filter
+     * @return array
+     */
+    public function fetchById(int $id, int $langId = 0)
+    {
+        return $this->findEntity($this->getColumns(), $id, $langId);
+    }
+
+    /**
+     * Fetch all dictionary entries
+     * 
+     * @param int $langId Language ID filter
+     * @return array
+     */
+    public function fetchAll(int $langId) : array
+    {
+        return $this->createEntitySelect($this->getColumns())
+                    ->whereEquals(DictionaryTranslationMapper::getFullColumnName('lang_id'), $langId)
+                    ->orderBy($this->getPk())
+                    ->desc()
+                    ->queryAll();
+    }
 }
