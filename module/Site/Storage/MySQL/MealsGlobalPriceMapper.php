0ed27cbf699a9cb39fc79e51f2ce279bc084fa2a
dave-yang <daworld.ny@gmail.com>
2018-03-08 08:09:58 +0500

    Finished MealsGlobalPriceMapper

--- module/Site/Storage/MySQL/MealsGlobalPriceMapper.php
+++ module/Site/Storage/MySQL/MealsGlobalPriceMapper.php
@@ -13,4 +13,46 @@ final class MealsGlobalPriceMapper extends AbstractMapper
     {
         return self::getWithPrefix('velveto_meals_global_prices');
     }
+
+    /**
+     * Update price
+     * 
+     * @param int $hotelId
+     * @param array $data
+     * @return boolean
+     */
+    public function updateRelation(int $hotelId, array $data) : bool
+    {
+        $this->deleteByColumn('hotel_id', $hotelId);
+
+        return $this->db->insertMany(self::getTableName(), ['hotel_id', 'price_group_id', 'price'], $data)
+                        ->execute();
+    }
+
+    /**
+     * Finds price group by hotel ID
+     * 
+     * @param int $hotelId
+     * @return array
+     */
+    public function findByHotelId(int $hotelId) : array
+    {
+        // Columns to be selected
+        $columns = [
+            PriceGroupMapper::column('id') => 'price_group_id',
+            PriceGroupMapper::column('currency'),
+            PriceGroupMapper::column('name') => 'group',
+            self::column('id'),
+            self::column('price')
+        ];
+
+        return $this->db->select($columns)
+                        ->from(PriceGroupMapper::getTableName())
+                        // Global price relation
+                        ->leftJoin(self::getTableName(), [
+                            self::column('price_group_id') => new RawSqlFragment(PriceGroupMapper::column('id')),
+                            self::column('hotel_id') => $hotelId
+                        ])
+                        ->queryAll();
+    }
 }
