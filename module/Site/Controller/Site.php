321c5116a5061110bcdbfebc6b7d467512bfa23c
dave-yang <daworld.ny@gmail.com>
2018-03-17 18:10:37 +0500

    Load Gmap

--- module/Site/Controller/Site.php
+++ module/Site/Controller/Site.php
@@ -173,6 +173,10 @@ final class Site extends AbstractSiteController
             $region = null;
         }
 
+        // Load map plugin
+        $this->view->getPluginBag()
+                   ->load(['map']);
+
         return $this->view->render('search', [
             'region' => $region,
 
