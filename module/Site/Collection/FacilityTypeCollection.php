4488c54d4d76e953cdef5d25ba242bb4a5fe2a04
dave-yang <daworld.ny@gmail.com>
2018-03-26 14:56:31 +0500

    Updates in facility type collections

--- module/Site/Collection/FacilityTypeCollection.php
+++ module/Site/Collection/FacilityTypeCollection.php
@@ -6,11 +6,16 @@ use Krystal\Stdlib\ArrayCollection;
 
 final class FacilityTypeCollection extends ArrayCollection
 {
+    const TYPE_FREE = '1';
+    const TYPE_CHARGED = '0';
+    const TYPE_YEAR = '2';
+
     /**
      * {@inheritDoc}
      */
     protected $collection = array(
-        '1' => 'Yes, free of charge', 
-        '0' => 'Yes, but not free'
+        self::TYPE_FREE => 'Yes, free of charge', 
+        self::TYPE_CHARGED => 'Yes, but not free',
+        self::TYPE_YEAR => 'Works within a year'
     );
 }
