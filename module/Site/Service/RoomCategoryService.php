a7b0b773a5179c028682a084af467f5fda66350a
dave-yang <daworld.ny@gmail.com>
2018-04-08 15:21:23 +0500

    Added fetchFilteredList()

--- module/Site/Service/RoomCategoryService.php
+++ module/Site/Service/RoomCategoryService.php
@@ -74,6 +74,31 @@ final class RoomCategoryService
      * Fetch all room categories
      * 
      * @param int $langId Language ID filter
+     * @param int $hotelId Hotel ID filter
+     * @param mixed $activeCategoryId Active category ID to be excluded
+     * @return array
+     */
+    public function fetchFilteredList(int $langId, int $hotelId, $activeCategoryId) : array
+    {
+        $list = $this->fetchList($langId);
+        $attached = $this->roomCategoryMapper->findAttachedIds($hotelId);
+
+        if (in_array($activeCategoryId, $attached)) {
+            // Delete by value
+            $key = array_search($activeCategoryId, $attached);
+
+            if (false !== $key) {
+                unset($attached[$key]);
+            }
+        }
+
+        return ArrayUtils::arrayWithout($list, $attached);
+    }
+
+    /**
+     * Fetch all room categories
+     * 
+     * @param int $langId Language ID filter
      * @return array
      */
     public function fetchList(int $langId)
