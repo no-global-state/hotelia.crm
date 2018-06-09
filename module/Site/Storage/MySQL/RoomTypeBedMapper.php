e0213ff556c9fe08293ae2c11899ab60793e3291
dave-yang <daworld.ny@gmail.com>
2018-06-09 19:38:55 +0500

    Added methods to fetch beds

--- module/Site/Storage/MySQL/RoomTypeBedMapper.php
+++ module/Site/Storage/MySQL/RoomTypeBedMapper.php
@@ -11,4 +11,54 @@ final class RoomTypeBedMapper extends AbstractMapper
     {
         return self::getWithPrefix('velveto_room_type_beds');
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getTranslationTable()
+    {
+        return RoomTypeBedTranslationMapper::getTableName();
+    }
+
+    /**
+     * Returns shared columns to be selected
+     * 
+     * @return array
+     */
+    private function getColumns() : array
+    {
+        return [
+            self::column('id'),
+            self::column('order'),
+            RoomTypeBedTranslationMapper::column('lang_id'),
+            RoomTypeBedTranslationMapper::column('name'),
+        ];
+    }
+
+    /**
+     * Fetch bed entry by its ID
+     * 
+     * @param int $id Entry ID
+     * @param int $langId Language ID filter
+     * @return array
+     */
+    public function fetchById(int $id, int $langId = 0)
+    {
+        return $this->findEntity($this->getColumns(), $id, $langId);
+    }
+
+    /**
+     * Fetch all bed entries
+     * 
+     * @param int $langId Language ID filter
+     * @return array
+     */
+    public function fetchAll(int $langId) : array
+    {
+        return $this->createEntitySelect($this->getColumns())
+                    ->whereEquals(DictionaryTranslationMapper::column('lang_id'), $langId)
+                    ->orderBy($this->getPk())
+                    ->desc()
+                    ->queryAll();
+    }
 }
