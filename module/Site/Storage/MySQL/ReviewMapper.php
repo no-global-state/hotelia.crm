bce1c2ac08a9c86ba31b79e12bc0a3c1d80a30f7
dave-yang <daworld.ny@gmail.com>
2018-07-13 02:53:25 +0500

    Added fetchAverages()

--- module/Site/Storage/MySQL/ReviewMapper.php
+++ module/Site/Storage/MySQL/ReviewMapper.php
@@ -2,6 +2,8 @@
 
 namespace Site\Storage\MySQL;
 
+use Krystal\Db\Sql\RawSqlFragment;
+
 final class ReviewMapper extends AbstractMapper
 {
     /**
@@ -10,6 +12,48 @@ final class ReviewMapper extends AbstractMapper
     public static function getTableName()
     {
         return self::getWithPrefix('velveto_reviews');
+    }
+
+    /**
+     * Fetch averages
+     * 
+     * @param int $hotelId
+     * @param int $langId
+     * @return array
+     */
+    public function fetchAverages(int $hotelId, int $langId) : array
+    {
+        // Columns to be selected
+        $columns = [
+            ReviewTypeMapper::column('id'),
+            ReviewTypeTranslationMapper::column('name'),
+            new RawSqlFragment('ROUND(AVG(velveto_reviews_marks.mark), 1) AS mark_avg')
+        ];
+
+        $db = $this->db->select($columns)
+                        ->from(ReviewTypeMapper::getTableName())
+                        // Type translation relation
+                        ->leftJoin(ReviewTypeTranslationMapper::getTableName(), [
+                            ReviewTypeTranslationMapper::column('id') => ReviewTypeMapper::getRawColumn('id')
+                        ])
+                        // Mark relation
+                        ->leftJoin(ReviewMarkMapper::getTableName(), [
+                            ReviewMarkMapper::column('review_type_id') => ReviewTypeMapper::getRawColumn('id')
+                        ])
+                        // Reviews relation
+                        ->leftJoin(self::getTableName(), [
+                            self::column('id') => ReviewMarkMapper::column('review_id'),
+                            ReviewTypeTranslationMapper::column('lang_id') => self::getRawColumn('lang_id')
+                        ])
+                        // Constraints
+                        ->whereEquals(ReviewTypeTranslationMapper::column('lang_id'), $langId)
+                        ->andWhereEquals(self::column('hotel_id'), $hotelId)
+                        ->groupBy([
+                            ReviewTypeMapper::column('id'),
+                            ReviewTypeTranslationMapper::column('name'),
+                        ]);
+
+        return $db->queryAll();
     }
 
     /**
