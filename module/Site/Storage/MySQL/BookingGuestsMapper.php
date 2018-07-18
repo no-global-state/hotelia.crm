4981f932f522d8508f2792352d7effca9b012edd
dave-yang <daworld.ny@gmail.com>
2018-07-18 23:25:52 +0500

    Added findByBookingId()

--- module/Site/Storage/MySQL/BookingGuestsMapper.php
+++ module/Site/Storage/MySQL/BookingGuestsMapper.php
@@ -11,4 +11,18 @@ final class BookingGuestsMapper extends AbstractMapper
     {
         return self::getWithPrefix('velveto_bookings_guests');
     }
+
+    /**
+     * Find rows by attached booking ID
+     * 
+     * @param int $bookingId
+     * @return array
+     */
+    public function findByBookingId(int $bookingId) : array
+    {
+        return $this->db->select('*')
+                        ->from(self::getTableName())
+                        ->whereEquals('booking_id', $bookingId)
+                        ->queryAll();
+    }
 }
