558798f188a8c2cc5bbbd51200ca82989eadc187
dave-yang <daworld.ny@gmail.com>
2018-07-18 22:22:51 +0500

    Added findDetailsByBookingId()

--- module/Site/Storage/MySQL/BookingRoomMapper.php
+++ module/Site/Storage/MySQL/BookingRoomMapper.php
@@ -11,4 +11,39 @@ final class BookingRoomMapper extends AbstractMapper
     {
         return self::getWithPrefix('velveto_bookings_rooms');
     }
+
+    /**
+     * Find details by booking ID
+     * 
+     * @param int $bookingId
+     * @param int $langId
+     * @return array
+     */
+    public function findDetailsByBookingId(int $bookingId, int $langId) : array
+    {
+        $columns = [
+            self::column('qty'),
+            self::column('guests'),
+            RoomCategoryTranslationMapper::column('name') => 'category'
+        ];
+
+        return $this->db->select($columns)
+                        ->from(self::getTableName())
+                        // Room type relation
+                        ->innerJoin(RoomTypeMapper::getTableName(), [
+                            RoomTypeMapper::column('id') => self::getRawColumn('room_type_id')
+                        ])
+                        // Category relation
+                        ->leftJoin(RoomCategoryMapper::getTableName(), [
+                            RoomCategoryMapper::column('id') => RoomTypeMapper::getRawColumn('category_id')
+                        ])
+                        // Category translation relation
+                        ->leftJoin(RoomCategoryTranslationMapper::getTableName(), [
+                            RoomCategoryTranslationMapper::column('id') => RoomCategoryMapper::getRawColumn('id')
+                        ])
+                        // Constraints
+                        ->whereEquals(RoomCategoryTranslationMapper::column('lang_id'), $langId)
+                        ->andWhereEquals(self::column('booking_id'), $bookingId)
+                        ->queryAll();
+    }
 }
