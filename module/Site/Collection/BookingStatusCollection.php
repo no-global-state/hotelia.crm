e3d0e160d87893c7296560beda1f06b687ccaa9f
dave-yang <daworld.ny@gmail.com>
2018-07-22 20:16:23 +0500

    Added BookingStatusCollection

---
+++ module/Site/Collection/BookingStatusCollection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Site\Collection;
+
+use Krystal\Stdlib\ArrayCollection;
+
+final class BookingStatusCollection extends ArrayCollection
+{
+    /* Status codes */
+    const STATUS_NEW = 0;
+    const STATUS_CONFIRMED = 1;
+    const STATUS_REJECTED = 2;
+    const STATUS_AWAITING_PAYMENT = 3;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $collection = [
+        self::STATUS_NEW => 'New',
+        self::STATUS_CONFIRMED => 'Confirmed',
+        self::STATUS_REJECTED => 'Rejected',
+        self::STATUS_AWAITING_PAYMENT => 'Awaiting payment'
+    ];
+}
