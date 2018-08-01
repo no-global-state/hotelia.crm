64d3690a6c9f03bb2b35ca933a66fd1817ff3ed2
dave-yang <daworld.ny@gmail.com>
2018-08-01 20:48:32 +0500

    Internal improvement in DaysCollection

--- module/Site/Collection/DaysCollection.php
+++ module/Site/Collection/DaysCollection.php
@@ -6,13 +6,19 @@ use Krystal\Stdlib\ArrayCollection;
 
 final class DaysCollection extends ArrayCollection
 {
+    // Day constants
+    const PARAM_WEEK_DAYS = 7;
+    const PARAM_TWO_WEEK_DAYS = 14;
+    const PARAM_THREE_WEEKS = 21;
+    const PARAM_MONTH = 31;
+
     /**
      * {@inheritDoc}
      */
-    protected $collection = array(
-        '7' => 'Week',
-        '14' => 'Two weeks',
-        '21' => 'Three weeks',
-        '31' => 'Month'
-    );
+    protected $collection = [
+        self::PARAM_WEEK_DAYS => 'Week',
+        self::PARAM_TWO_WEEK_DAYS => 'Two weeks',
+        self::PARAM_THREE_WEEKS => 'Three weeks',
+        self::PARAM_MONTH => 'Month'
+    ];
 }
