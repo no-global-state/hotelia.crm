d28a82473dbd951eccbb4b16a3c13b7e6ab4e971
dave-yang <daworld.ny@gmail.com>
2018-08-02 00:58:40 +0500

    Replaced stat mockups with actual data

--- module/Site/Controller/Stat.php
+++ module/Site/Controller/Stat.php
@@ -20,6 +20,7 @@ final class Stat extends AbstractCrmController
         return $this->view->render('stat/index', [
             'icon' => 'glyphicon glyphicon-stats',
             'stat' => true,
+            'data' => $this->getModuleService('reservationService')->getStatistic($this->getHotelId()),
             'months' => TimeHelper::getMonths(),
             'year' => date('Y'),
             'priceGroups' => $this->getModuleService('priceGroupService')->fetchList(),
