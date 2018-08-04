25955efc72138a38dbe8d9093454f114685efd22
dave-yang <daworld.ny@gmail.com>
2018-08-04 15:47:34 +0500

    Added initial mailer methods and their templates

--- module/Site/Controller/MailerTrait.php
+++ module/Site/Controller/MailerTrait.php
@@ -7,6 +7,102 @@ use Site\Service\MailerService;
 trait MailerTrait
 {
     /**
+     * Notify administration about new booking
+     * 
+     * @param string $hotelName
+     * @return boolean
+     */
+    protected function bookingAdminNotify(string $hotelName) : bool
+    {
+        $subject = 'New booking from the site';
+        $to = $_ENV['adminEmail'];
+
+        return $this->sendMail($to, $subject, 'booking-owner-new', [
+            'hotelName' => $hotelName
+        ]);
+    }
+
+    /**
+     * Notify about new booking
+     * 
+     * @param string $to
+     * @return boolean
+     */
+    protected function bookingOwnerNotify(string $to) : bool
+    {
+        $subject = 'You have a new booking from site';
+        return $this->sendMail($to, $subject, 'booking-owner-new');
+    }
+
+    /**
+     * Notify about new feedback
+     * 
+     * @param string $to
+     * @return boolean
+     */
+    protected function feedbackNewNotify(string $to)
+    {
+        $subject = 'You have a new feedback';
+        return $this->sendMail($to, $subject, 'feedback-new');
+    }
+
+    /**
+     * Notify about booking being processed
+     * 
+     * @param string $to
+     * @return boolean
+     */
+    protected function bookingProcessedNotify(string $to)
+    {
+        $subject = 'Your booking is being processed';
+        return $this->sendMail($to, $subject, 'booking-on-moderation');
+    }
+
+    /**
+     * Emails about the need of payment confirmation
+     * 
+     * @param string $to
+     * @param string $link Payment confirmation link
+     * @return boolean
+     */
+    protected function paymentNeedsConfirmNotify(string $to, string $link) : bool
+    {
+        $subject = 'Please confirm your payment';
+
+        return $this->sendMail($to, $subject, 'payment-to-be-confirmed', [
+            'link' => $link
+        ]);
+    }
+
+    /**
+     * Notify about successful payment
+     * 
+     * @param string $to
+     * @return boolean
+     */
+    protected function paymentSuccessNotify(string $to) : bool
+    {
+        $subject = 'Your booking is complete';
+        return $this->sendMail($to, $subject, 'payment-confirmed');
+    }
+
+    /**
+     * Notify admin about new transaction
+     * 
+     * @param string $hotelName
+     * @return boolean
+     */
+    protected function transactionAdminNotify(string $hotelName) : bool
+    {
+        $subject = 'New transaction';
+        $to = $_ENV['adminEmail'];
+
+        return $this->sendMail($to, $subject, 'payment-transaction', [
+            'hotelName' => $hotelName
+        ]);
+    }
+
+    /**
      * Renders mail message
      * 
      * @param string $template Framework-compliant template name
