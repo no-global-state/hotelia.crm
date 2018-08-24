0a75a2c9cff474cd4ed8b638f42b27b4f10e6174
dave-yang <daworld.ny@gmail.com>
2018-08-24 06:47:40 +0500

    Turn raw values into constants

--- module/Site/Collection/RoomQualityCollection.php
+++ module/Site/Collection/RoomQualityCollection.php
@@ -6,15 +6,22 @@ use Krystal\Stdlib\ArrayCollection;
 
 final class RoomQualityCollection extends ArrayCollection
 {
+    const STATUS_UNDEFINED = 0;
+    const STATUS_PERFECT = 1;
+    const STATUS_GOOD = 2;
+    const STATUS_MEDIUM = 3;
+    const STATUS_BAD = 4;
+    const STATUS_ON_REPAIR = 5;
+
     /**
      * {@inheritDoc}
      */
-    protected $collection = array(
-        '0' => 'Undefined',
-        '1' => 'Perfect',
-        '2' => 'Good',
-        '3' => 'Medium',
-        '4' => 'Bad',
-        '5' => 'On repair'
-    );
+    protected $collection = [
+        self::STATUS_UNDEFINED => 'Undefined',
+        self::STATUS_PERFECT => 'Perfect',
+        self::STATUS_GOOD => 'Good',
+        self::STATUS_MEDIUM => 'Medium',
+        self::STATUS_BAD => 'Bad',
+        self::STATUS_ON_REPAIR => 'On repair'
+    ];
 }
