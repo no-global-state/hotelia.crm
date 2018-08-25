51d528bb8fbea6e597dab44b8d032cc327861f5b
dave-yang <daworld.ny@gmail.com>
2018-08-25 20:57:40 +0500

    Added findAllByExternalId()

--- module/Site/Storage/MySQL/BookingExternalRelationMapper.php
+++ module/Site/Storage/MySQL/BookingExternalRelationMapper.php
@@ -11,4 +11,46 @@ final class BookingExternalRelationMapper extends AbstractMapper
     {
         return self::getWithPrefix('velveto_bookings_external');
     }
+
+    /**
+     * Find all bookings by external user ID
+     * 
+     * @param int $id External user ID
+     * @return array
+     */
+    public function findAllByExternalId(int $id) : array
+    {
+        // Columns to be selected
+        $columns = [
+            BookingMapper::column('id'),
+            BookingMapper::column('hotel_id'),
+            BookingMapper::column('price_group_id'),
+            BookingMapper::column('lang_id'),
+            BookingMapper::column('datetime'),
+            BookingMapper::column('status'),
+            BookingMapper::column('arrival'),
+            BookingMapper::column('departure'),
+            BookingMapper::column('phone'),
+            BookingMapper::column('email'),
+            BookingMapper::column('comment'),
+            BookingMapper::column('near_preferred'),
+            BookingMapper::column('amount'),
+            PriceGroupMapper::column('name') => 'price_group',
+            PriceGroupMapper::column('currency'),
+        ];
+
+        $db = $this->db->select($columns)
+                       ->from(BookingMapper::getTableName())
+                       // Relational constraint
+                       ->innerJoin(self::getTableName(), [
+                            self::column('slave_id') => BookingMapper::getRawColumn('id')
+                       ])
+                       // Price group relation
+                       ->leftJoin(PriceGroupMapper::getTableName(), [
+                            BookingMapper::column('price_group_id') => PriceGroupMapper::getRawColumn('id')
+                       ])
+                       ->whereEquals(self::column('master_id'), $id);
+
+        return $db->queryAll();
+    }
 }
